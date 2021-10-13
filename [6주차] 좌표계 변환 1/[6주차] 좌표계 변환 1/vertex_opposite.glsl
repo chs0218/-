5a9fddbd39563565533c583104dd08aa109d1c38
Choi Hae Seong <39898936+chs0218@users.noreply.github.com>
2021-10-14 07:19:41 +0900

    ㅗㄴㄹㄴ옿ㄹㄴㅇ로

---
+++ [6주차] 좌표계 변환 1/[6주차] 좌표계 변환 1/vertex_opposite.glsl
@@ -0,0 +1,15 @@
+#version 330
+//--- in_Position: attribute index 0 
+//--- in_Color: attribute index 1
+
+layout (location = 0) in vec3 in_Position; 
+layout (location = 1) in vec3 in_Color;
+
+uniform mat4 modelTransform;
+
+out vec3 out_Color;
+void main(void) 
+{
+gl_Position = modelTransform * vec4 (in_Position.x, in_Position.y, -in_Position.z, 1.0);
+out_Color = in_Color;
+}
